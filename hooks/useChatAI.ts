277fdb0e99153fef871bad66a28919a5e1e3857e
finnkalus618-hub <finnkalus618@gmail.com>
2026-09-20 01:20:57 +0800

    Update useChatAI.ts

--- hooks/useChatAI.ts
+++ hooks/useChatAI.ts
@@ -1,4 +1,8 @@
-import { shouldWriteLocalMemory } from '../utils/memoryBackend';
+import {
+    isOmbreMemorySelected,
+    shouldWriteLocalMemory,
+} from '../utils/memoryBackend';
+import { saveOmbreMemory } from '../utils/ombreMemory';
 import { useState, useRef, useEffect, useSyncExternalStore, MutableRefObject } from 'react';
 import { CharacterProfile, UserProfile, Message, Emoji, EmojiCategory, GroupProfile, RealtimeConfig, CharacterBuff, Amsg2ExpiredNoticeRecord } from '../types';
 import { DB } from '../utils/db';
